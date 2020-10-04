f56494737ed8cf90e7c260a27c969d3bfd2a0033
rafahg <r.hgar78@gmail.com>
2020-10-04 15:16:17 +0100

    Calculator instance methods added

--- script.js
+++ script.js
@@ -23,15 +23,45 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-
+        if(this.currentOperand === "") return
+        if(this.previousOperand !== "") {
+            this.compute()
+        }
+        this.operation = operation 
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev)  || isNaN(current)) return 
+        switch (this.operation) {
+            case '+':
+                computation = prev + current 
+                break 
+            case '-':
+                computation = prev - current 
+                break
+            case '*':
+                computation = prev * current 
+                break 
+            case '÷': 
+                computation = prev / current 
+                break 
+            default: 
+                return 
 
+        }
+        this.currentOperand = computation
+        this.operation = undefined 
+        this.previousOperand = ''
     }
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
     }
 
 }
@@ -59,5 +89,21 @@ numberButtons.forEach(button => {
     })
 })
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
+equalsButton.addEventListener('click',button => {
+    calculator.compute()
+    calculator.updateDisplay()    
+})
+
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
 
